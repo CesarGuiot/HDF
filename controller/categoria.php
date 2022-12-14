a2ce9b56358783a4f0ffed831f5da442e76cd755
CESAR GUIOT <elguiot10@gmail.com>
2022-12-14 14:22:28 -0500

    Actualización datatble

--- controller/categoria.php
+++ controller/categoria.php
@@ -11,7 +11,7 @@
             if(is_array($datos)==true and count($datos)>0){
                 foreach($datos as $row)
                 {
-                    $html.= "<option value'".$row['cat_id']."'>".$row['cat_nom']."</option>";
+                    $html.= "<option value='".$row['cat_id']."'>".$row['cat_nom']."</option>";
                 }
                 echo $html;
             }
